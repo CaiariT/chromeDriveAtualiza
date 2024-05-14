3c7f05c7695ed63fc25e3b7f58c09d2cd7f6b609
CaiariT <caiari.tolotti@gmail.com>
2024-05-14 17:43:38 -0300

    up correação em verificar os arquivos

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ const unzipper = require('unzipper');
 
 const urlBase = 'https://storage.googleapis.com/chrome-for-testing-public/';
 
-
-function baixarEDescompactarArquivo(url, destino) {
-  return axios({
-    method: 'get',
-    url: url,
-    responseType: 'stream' 
-  })
-  .then(response => {
+// Função para baixar o arquivo e descompactá-lo
+async function baixarEDescompactarArquivo(url, destino) {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: url,
+      responseType: 'stream'
+    });
 
     const unzipStream = response.data.pipe(unzipper.Parse());
 
@@ -21,6 +21,7 @@ function baixarEDescompactarArquivo(url, destino) {
         const fileName = entry.path;
         const filePath = path.join(destino, fileName);
 
+        // Criar diretórios se o arquivo estiver em uma subpasta
         if (fileName.includes('/')) {
           fs.mkdirSync(path.dirname(filePath), { recursive: true });
         }
@@ -39,15 +40,14 @@ function baixarEDescompactarArquivo(url, destino) {
         reject(error);
       });
     });
-  })
-  .catch(error => {
+  } catch (error) {
     throw new Error(`Erro ao baixar e descompactar o arquivo: ${error.message}`);
-  });
+  }
 }
 
 const versionUrl = 'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json';
 axios.get(versionUrl)
-  .then(response => {
+  .then(async response => {
     const jsonData = response.data;
 
     if ('Stable' in jsonData.channels) {
@@ -58,25 +58,31 @@ axios.get(versionUrl)
       console.log('URL completa:', urlCompleta);
 
       const dirPath = process.env.DIR_PATH || path.join(__dirname, '..', 'docs');
+      console.log('Diretório de destino:', dirPath);
 
+      // Limpar o diretório de destino antes de extrair os arquivos
       fs.rmdirSync(dirPath, { recursive: true });
       console.log('Diretório de destino limpo:', dirPath);
 
+      // Criar o diretório de destino se não existir
       if (!fs.existsSync(dirPath)) {
         fs.mkdirSync(dirPath, { recursive: true });
         console.log('Pasta "docs" criada com sucesso.');
       }
 
-      baixarEDescompactarArquivo(urlCompleta, dirPath)
-        .catch(error => {
-          console.error('Erro ao baixar e descompactar o arquivo:', error.message);
-          process.exit(1);
-        });
+      await baixarEDescompactarArquivo(urlCompleta, dirPath);
+
+      // Verificar se os arquivos foram extraídos corretamente
+      const arquivosExtraidos = fs.readdirSync(dirPath);
+      if (arquivosExtraidos.length === 0) {
+        console.error('Nenhum arquivo encontrado na pasta de destino.');
+        process.exit(1);
+      }
     } else {
       console.log('Não existe versão estável disponível.');
     }
   })
   .catch(error => {
     console.error('Erro ao fazer solicitação HTTP:', error.message);
-    process.exit(1); 
+    process.exit(1); // Encerrar com código de erro
   });
